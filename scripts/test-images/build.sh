26043175ef48fd2e79c6956f8002da04371a9b05
Andrew Aladjev <aladjev.andrew@gmail.com>
2019-10-18 17:21:14 +0300

    added i686 test image

--- scripts/test-images/build.sh
+++ scripts/test-images/build.sh
@@ -4,3 +4,4 @@ set -e
 cd "$(dirname $0)"
 
 ./amd64-pc-linux-gnu/build.sh
+./i686-pc-linux-gnu/build.sh
--- scripts/test-images/pull.sh
+++ scripts/test-images/pull.sh
@@ -4,3 +4,4 @@ set -e
 cd "$(dirname $0)"
 
 ./amd64-pc-linux-gnu/pull.sh
+./i686-pc-linux-gnu/pull.sh
