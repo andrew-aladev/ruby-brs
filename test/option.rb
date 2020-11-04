3b3ebe7175737c0919403ebb51b59aa61cad79e0
Andrew Aladjev <aladjev.andrew@gmail.com>
2020-11-04 11:35:41 +0300

    testing modes cosmetics, removed unused options method

--- test/option.rb
+++ test/option.rb
@@ -185,12 +185,6 @@ module BRS
         buffer_length_generator.mix(main_generator).mix(thread_generator).mix other_generator
       end
 
-      def self.get_compressor_options(buffer_length_names, &block)
-        generator = get_compressor_options_generator buffer_length_names
-
-        generator.each(&block)
-      end
-
       def self.get_compatible_decompressor_options(compressor_options, buffer_length_name_mapping, &_block)
         decompressor_options = {
           :large_window => compressor_options[:large_window],
