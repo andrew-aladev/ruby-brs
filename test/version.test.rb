dd709ff2c63c5e3b02bd02b23ba2b14e10600f13
Andrew Aladjev <aladjev.andrew@gmail.com>
2022-07-04 19:11:32 +0300

    implementing adsp tests

--- test/version.test.rb
+++ test/version.test.rb
@@ -1,14 +1,15 @@
 # Ruby bindings for brotli library.
 # Copyright (c) 2019 AUTHORS, MIT License.
 
+require "adsp/test/version"
 require "brs"
 
 require_relative "minitest"
 
 module BRS
   module Test
-    class Version < Minitest::Test
-      def test_versions
+    class Version < ADSP::Test::Version
+      def test_version
         refute_nil BRS::VERSION
         refute_nil BRS::LIBRARY_VERSION
       end
