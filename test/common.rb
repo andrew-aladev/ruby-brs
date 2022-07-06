7f873df07c3b331c3b26e4e11b41852506cc6f09
Andrew Aladjev <aladjev.andrew@gmail.com>
2022-07-06 17:43:41 +0300

    updated tests

--- test/common.rb
+++ test/common.rb
@@ -5,8 +5,6 @@ require "parallel"
 require "securerandom"
 require "tempfile"
 
-require_relative "coverage_helper"
-
 module BRS
   module Test
     module Common
--- test/stream/minitar.test.rb
+++ test/stream/minitar.test.rb
@@ -1,47 +1,18 @@
 # Ruby bindings for brotli library.
 # Copyright (c) 2019 AUTHORS, MIT License.
 
+require "adsp/test/stream/minitar"
 require "brs/stream/reader"
 require "brs/stream/writer"
-require "minitar"
 
-require_relative "../common"
 require_relative "../minitest"
 
 module BRS
   module Test
     module Stream
-      class MinitarTest < Minitest::Test
+      class MinitarTest < ADSP::Test::Stream::MinitarTest
         Reader = BRS::Stream::Reader
         Writer = BRS::Stream::Writer
-
-        ARCHIVE_PATH = Common::ARCHIVE_PATH
-        LARGE_TEXTS  = Common::LARGE_TEXTS
-
-        def test_tar
-          Common.parallel LARGE_TEXTS do |text, worker_index|
-            archive_path = Common.get_path ARCHIVE_PATH, worker_index
-
-            Writer.open archive_path do |writer|
-              Minitar::Writer.open writer do |tar|
-                tar.add_file_simple "file", :data => text
-              end
-            end
-
-            Reader.open archive_path do |reader|
-              Minitar::Reader.open reader do |tar|
-                tar.each_entry do |entry|
-                  assert_equal "file", entry.name
-
-                  decompressed_text = entry.read
-                  decompressed_text.force_encoding text.encoding
-
-                  assert_equal text, decompressed_text
-                end
-              end
-            end
-          end
-        end
       end
 
       Minitest << MinitarTest
